34c71e39b9b38c7a232590a9549fe5e3fe3083a2
Natan Santolo <natan.santolo@gmail.com>
2017-04-04 09:12:24 +0200

    Added layout options for Receipt component

--- src/receipt.js
+++ src/receipt.js
@@ -1,6 +1,30 @@
 import * as handlebars from './receipt.handlebars';
 import * as styles from 'raw-loader!./receipt.css';
 
+function getDefaultLayout() {
+  return {
+    "HEADER": {
+      "BRAND": true,
+      "TITLE": true,
+      "MESSAGE": true,
+      "DETAILS": true
+    },
+    "CONTENT": {
+      "LINEITEMS": true,
+      "LINETAXES": true,
+      "TOTALS": true,
+      "WALLET": true,
+      "DOWNLOAD": true
+    },
+    "FOOTER": {
+      "MAP": true,
+      "CONTACT": true,
+      "ADDRESS": true,
+      "MESSAGE": true
+    }
+  }
+}
+
 /**
  * @class Receipt
  * @extends HTMLElement
@@ -15,12 +39,17 @@ class Receipt extends HTMLElement {
 
   // Called every time the element is inserted into the DOM.
   connectedCallback() {
+    // get layout information
+    let layout = this.getLayout();
     // fetch data
     let data = this.getData();
     if (!data) {
       throw 'Receipt: Data is missing, component needs <pre id="data"></pre> to initialize';
       return;
     }
+    // store layout information in template data
+    data.LAYOUT = layout;
+    console.log('processing tempalte with data', data);
     // process and render template
     this.shadow.innerHTML = handlebars(data);
     // attach stylesheet
@@ -36,8 +65,29 @@ class Receipt extends HTMLElement {
 
   // Get data from <pre id="data"></pre>
   getData() {
-    let d = this.shadow.host.querySelector('#data');
+    let d = this.shadow.host.querySelector('data');
     return d.innerHTML ? JSON.parse(d.innerHTML) : false;
+  }
+
+  getLayout() {
+    let layout = {};
+    let layoutElement = this.shadow.host.querySelector('layout');
+
+    if (!layoutElement) {
+      return getDefaultLayout();
+    }
+
+    function extractChildren(element) {
+      if (!element.children.length) return element;
+      let children = {};
+      for (let child, i = 0; i < element.children.length; i += 1) {
+        child = element.children[i];
+        children[child.tagName] = extractChildren(child);
+      }
+      return children;
+    }
+
+    return extractChildren(layoutElement);
   }
 
 // End Receipt
