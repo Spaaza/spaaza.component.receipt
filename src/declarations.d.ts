2db7624dea9f7122020d815f61382f420eefada5
Arthur Langereis <puurtuur@me.com>
2017-08-30 16:24:33 +0200

    json content is any

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,4 +3,9 @@ declare module "*.less" {
     export default content;
 }
 
+declare module "*.json" {
+    const content: any;
+    export default content;
+}
+
 declare function require(moduleName: string): any;
