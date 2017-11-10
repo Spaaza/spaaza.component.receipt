1652dafc2c8bbb2be1f8ee9f562470dec6adfb69
Arthur Langereis <puurtuur@me.com>
2017-11-10 16:11:40 +0100

    convert to basic self-loading faux-webcomponent
    - adds Firefox support
    - adds IE 11 support
    - adds old iOS / Android support

--- src/receipt.ts
+++ src/receipt.ts
@@ -5,15 +5,21 @@ import renderReceipt from "./component/receipt";
 import renderError from "./component/error";
 import styles from "./receipt.less";
 
-let root: HTMLElement;
-
-function connect() {
-	redraw();
+function connectAll() {
+	const receipts: HTMLElement[] = [].slice.call(document.querySelectorAll("spaaza-receipt"), 0);
+	for (const receipt of receipts) {
+		if (! receipt.querySelector(".receipt-body")) {
+			const rbody = document.createElement("div");
+			rbody.className = "receipt-body";
+			receipt.appendChild(rbody);
+			redraw(receipt, rbody);
+		}
+	}
 }
 
-function getConfig() {
+function getConfig(host: HTMLElement) {
 	const getJSONBlock = (type: string) => {
-		const dataNode = root.querySelector(`script[data-${type}]`);
+		const dataNode = host.querySelector(`script[data-${type}]`);
 		if (dataNode && dataNode.textContent) {
 			try {
 				return JSON.parse(dataNode.textContent);
@@ -25,7 +31,7 @@ function getConfig() {
 	};
 
 	// get the built-in and user provided strings
-	const lang = root.getAttribute("language") || "";
+	const lang = host.getAttribute("language") || "";
 	const langStrings = getStrings(lang);
 	const userStrings = getJSONBlock("strings");
 	const combinedStrings = (userStrings) ? overrideStrings(langStrings, userStrings) : langStrings;
@@ -38,8 +44,8 @@ function getConfig() {
 	};
 }
 
-function redraw() {
-	const config = getConfig();
+function redraw(host: HTMLElement, root: HTMLElement) {
+	const config = getConfig(host);
 	let contents: string;
 	if (config.receipt) {
 		const data = parseReceipt(config.receipt);
@@ -61,10 +67,14 @@ function redraw() {
 	root.innerHTML = `<style>${styles}</style>\n<div class="spaaza-receipt">${contents}</div>`;
 }
 
+
 if (document.readyState === "loading") {
 	document.addEventListener("readystatechange", () => {
 		if (document.readyState !== "loading") {
-			connect();
+			connectAll();
 		}
 	});
+}
+else {
+	connectAll();
 }
