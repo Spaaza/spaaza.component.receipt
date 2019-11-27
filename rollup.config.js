84549ea1830b8571ea2a25390c5b22dab0831267
Arthur Langereis <puurtuur@me.com>
2019-11-27 17:39:09 +0100

    upgrade build deps and fix pkg & build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import cleanCSS from "less-plugin-clean-css";
 import less from "rollup-plugin-less";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import typescript from "typescript";
 import tsc from "rollup-plugin-typescript2";
-import uglify from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 
 const pkg = require("./package.json");
 
@@ -28,7 +28,7 @@ export default [
 				]
 			}),
 			json({
-				include: 'src/lang/**',				
+				include: 'src/lang/**',
 			}),
 			tsc({
 				typescript: typescript,
